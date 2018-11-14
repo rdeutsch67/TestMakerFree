cd45e1140eb296656e29b5110ee157e786bb15ae
rdeutsch67 <robert.deutschmann@gmx.ch>
2018-11-14 23:07:48 +0100

    ok

--- TestMakerFreeApp/ClientApp/src/app/app.module.ts
+++ TestMakerFreeApp/ClientApp/src/app/app.module.ts
@@ -7,18 +7,16 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { QuizListComponent } from './components/quiz-list.component';
+import { QuizListComponent } from './components/quiz-list/quiz-list.component';
+import {QuizComponent} from './components/quiz/quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    QuizListComponent
+    QuizListComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,8 +24,10 @@ import { QuizListComponent } from './components/quiz-list.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
+      /*
+{ path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+*/
     ])
   ],
   providers: [],
